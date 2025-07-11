8162643fbc7eb87a0e35b961dc6a9db66426876a
shweta074 <rajputshweta074@gmail.com>
2025-07-11 09:53:46 +0530

    init

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    Blog_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    Blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
